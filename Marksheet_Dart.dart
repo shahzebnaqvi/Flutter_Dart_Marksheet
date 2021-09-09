d9719ba6bbbe368fcc9e7b8aa08087879449acca
shahzebnaqvi <shahzebnaqvi12@gmail.com>
2021-09-09 18:01:02 +0500

    variable declare and some calculation

---
+++ Marksheet_Dart.dart
@@ -0,0 +1,13 @@
+void main() {
+  var maths, english, urdu, physics, science, total , percentage;
+  maths=90;
+  english=70;
+  urdu= 83; 
+  physics=94;
+  science=60;
+  total = maths + english + urdu + physics + science ; 
+  percentage =  total/500 *100;
+  print(total);
+  print(percentage);
+
+}
